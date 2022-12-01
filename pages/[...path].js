fa24f53aed2c634079c9c88b59a247d59928fcf0
rsemenyshyn <r.semenyshyn.ua@gmail.com>
2022-12-02 00:19:28 +0100

    minor fixes

--- pages/[...path].js
+++ pages/[...path].js
@@ -1,30 +1,22 @@
-import GetSitemapLinks from 'sitemap-links'
-import DynamicPath, { getStaticProps } from './index'
+import GetSitemapLinks from 'sitemap-links';
+import DynamicPath, { getStaticProps } from './index';
 
-export default DynamicPath
+export default DynamicPath;
 
-export { getStaticProps }
+export { getStaticProps };
 
 export async function getStaticPaths() {
 	// Fetch links from Webflow sitemap
-	const sitemapLink = process.env.WEBFLOW_URL + `/sitemap.xml`
-	const links = await GetSitemapLinks(sitemapLink).catch(err => {
-		console.error(err)
-	})
+	const sitemapLink = process.env.WEBFLOW_URL + `/sitemap.xml`;
+	const links = await GetSitemapLinks(sitemapLink).catch(err => console.error(err));
 
 	// Extract paths from absolute links
-	const paths = []
+	const paths = [];
 	for(let link of links){
-		let url = new URL(link)
-		const path = url.pathname.replace(`/`, ``).split(`/`)
+		let url = new URL(link);
+		const path = url.pathname.replace(`/`, ``).split(`/`);
 		if(!path.length || !path[0]) continue
-		paths.push({
-			params: { path }
-		})
+		paths.push({ params: { path }});
 	}
-
-	return {
-	  paths: paths,
-	  fallback: `blocking`,
-	}
+	return { paths: paths, fallback: `blocking` };
  }
--- pages/index.js
+++ pages/index.js
@@ -1,116 +1,100 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import parseHtml, { domToReact } from 'html-react-parser'
-import get from 'lodash/get'
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+
+import get from 'lodash/get';
+import parseHtml, { domToReact } from 'html-react-parser';
 
 // Determines if URL is internal or external
 function isUrlInternal(link){
-  if(
-    !link ||
-    link.indexOf(`https:`) === 0 ||
-    link.indexOf(`#`) === 0 ||
-    link.indexOf(`http`) === 0 ||
-    link.indexOf(`://`) === 0
-  ){
-    return false
-  }
-  return true
+	return !(!link ||
+		link.indexOf(`https:`) === 0 ||
+		link.indexOf(`#`) === 0 ||
+		link.indexOf(`http`) === 0 ||
+		link.indexOf(`://`) === 0);
 }
 
 // Replaces DOM nodes with React components
 function replace(node){
-  const attribs = node.attribs || {}
+	const attribs = node.attribs || {};
+	const { style, ...props } = attribs;
 
-  // Replace links with Next links
-  if(node.name === `a` && isUrlInternal(attribs.href)){
-    const { href, style, ...props } = attribs
-    if(props.class){
-      props.className = props.class
-      delete props.class
-    }
-    if(!style){
-      return (
-        <Link href={href}>
-          <a {...props}>
-            {!!node.children && !!node.children.length &&
-              domToReact(node.children, parseOptions)
-            }
-          </a>
-        </Link>
-      )
-    }
-    return (
-      <Link href={href}>
-        <a {...props} href={href} css={style}>
-          {!!node.children && !!node.children.length &&
-            domToReact(node.children, parseOptions)
-          }
-        </a>
-      </Link>
-    )
-  }
+	if(node.name === `a` && isUrlInternal(attribs.href)) {
+		// Replace links with Next links
+		if(props.class){
+			props.className = props.class;
+			delete props.class;
+		}
+		return (
+			<Link href={attribs.href}>
+				<a {...props} css={style ? style : null}>
+					{!!node.children && !!node.children.length && domToReact(node.children, parseOptions)}
+				</a>
+			</Link>
+		)
+	}
 
+	if(node.name === `script`){
+		// Make Google Fonts scripts work
+		let content = get(node, `children.0.data`, ``);
+		if(content && content.trim().indexOf(`WebFont.load(`) === 0){
+			content = `setTimeout(function(){${content}}, 1)`;
+			return (
+				<script {...attribs} dangerouslySetInnerHTML={{__html: content}}></script>
+			)
+		}
+	}
 
-  // Make Google Fonts scripts work
-  if(node.name === `script`){
-    let content = get(node, `children.0.data`, ``)
-    if(content && content.trim().indexOf(`WebFont.load(`) === 0){
-      content = `setTimeout(function(){${content}}, 1)`
-      return (
-        <script {...attribs} dangerouslySetInnerHTML={{__html: content}}></script>
-      )
-    }
-  }
+	if(node.name === `form`) {
+		// Make forms work, if action is not set
+		if(props.class){
+			props.className = props.class;
+			delete props.class;
+		}
+		const formAction = attribs.action ? attribs.action : '/';
+		return (
+			<form {...props} action={formAction} css={style ? style : null}>
+				{!!node.children && !!node.children.length && domToReact(node.children, parseOptions)}
+			</form>
+		);
+	}
 
 }
 const parseOptions = { replace }
 
 export default function Home(props) {
-  return (
-    <>
-      <Head>
-        {parseHtml(props.headContent, parseOptions)}
-      </Head>
-      {parseHtml(props.bodyContent, parseOptions)}
-    </>
-  )
+	return (
+		<>
+			<Head>
+				{parseHtml(props.headContent, parseOptions)}
+			</Head>
+			{parseHtml(props.bodyContent, parseOptions)}
+		</>
+	)
 }
 
 export async function getStaticProps(ctx) {
-  // Import modules in here that aren't needed in the component
-  const cheerio = await import(`cheerio`)
-  const axios = (await import(`axios`)).default
+	// Import modules in here that aren't needed in the component
+	const cheerio = await import(`cheerio`);
+	const axios = (await import(`axios`)).default;
 
+	// Use path to determine Webflow path
+	let url = get(ctx, `params.path`, []);
+	url = url.join(`/`);
+	url = url.charAt(0) !== `/` ? `/${url}` : url;
+	const fetchUrl = process.env.WEBFLOW_URL + url;
 
-  // Use path to determine Webflow path
-  let url = get(ctx, `params.path`, [])
-  url = url.join(`/`)
-  if(url.charAt(0) !== `/`){
-    url = `/${url}`
-  }
-  const fetchUrl = process.env.WEBFLOW_URL + url
+	// Fetch HTML
+	let res = await axios(fetchUrl).catch(err => console.error(err));
+	const html = res.data;
 
-  // Fetch HTML
-  let res = await axios(fetchUrl)
-    .catch(err => {
-      console.error(err)
-    })
-  const html = res.data
+	// Parse HTML with Cheerio
+	const $ = cheerio.load(html);
 
+	// Convert back to HTML strings
+	const bodyContent = $(`body`).html();
+	const headContent = $(`head`).html();
 
-  // Parse HTML with Cheerio
-  const $ = cheerio.load(html)
-
-  // Convert back to HTML strings
-  const bodyContent = $(`body`).html()
-  const headContent = $(`head`).html()
-
-  // Send HTML to component via props
-  return {
-    props: {
-      bodyContent,
-      headContent
-    },
-  }
+	// Send HTML to component via props
+	return { props: { bodyContent, headContent }};
 }
